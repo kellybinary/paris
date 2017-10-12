b96b080e7d28cac1870e769b1c1e581815db4378
zakibinary <zaki@Zakis-MacBook-Pro-2.local>
2017-10-12 13:35:23 +0800

    Remove _site folder

--- _site/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    setActiveMenu();
-});
-
-function setActiveMenu() {
-    var $m = $('#nav-content li.nav-item');
-    $m.removeClass('active');
-    $m.find('a.nav-link[href$="' + window.location.pathname.toLowerCase().replace(/\/$/, '') + '"]').parent('.nav-item').addClass('active');
-}
