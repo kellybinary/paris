fb47870d929a1ab0fc5d10cc0c49ba848ae26bd7
zakibinary <zaki@Zakis-MacBook-Pro-2.local>
2017-10-13 10:15:36 +0800

    updated template and styling

--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@ function setActiveMenu() {
     $m.removeClass('active');
     $m.find('a.nav-link[href$="' + window.location.pathname.toLowerCase().replace(/\/$/, '') + '"]').parent('.nav-item').addClass('active');
 }
+
+$('#goTop').on('click', function(e){
+    $("html, body").animate({scrollTop: $("#top").offset().top}, 500);
+});
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 500) {
+        $('#goTop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('#goTop').stop(true, true).fadeOut();
+    }
+});
